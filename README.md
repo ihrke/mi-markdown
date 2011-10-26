dc18885fb5108b98d256f9df07c1fa1e7ad20187
Matthias Ihrke <ihrke@nld.ds.mpg.de>
2011-10-26 20:48:22 +0200

    added css/header/footer capability

--- README.md
+++ README.md
@@ -3,25 +3,22 @@
 (see [raw version](https://raw.github.com/ihrke/mi-markdown/master/README.md))
 
 ## Files ##
-* [markd.py]() - executable to turn markdown into HTML
-* [mdx_pygments.py]() - python-markdown extension for syntax-highlighting a'la github
+* [markd.py](markd.py) - executable to turn markdown into HTML
+* [mdx_pygments.py](mdx_pygments.py) - python-markdown extension for syntax-highlighting a'la github
                  
 ## Features ##
 
-* github-style fenced code with syntax highlighting:
-
-    \`\`\`python
-    def func(a):
-       print a
-    \`\`\`
-   
-becomes
-
-```python
-def func(a):
-   print a
-```
-
+* github-style fenced code with syntax highlighting
+        ```python
+        def func(a):
+           print a
+        ```
+* works with all code types supported by pygments, e.g.
+        ```c
+        void main(int argc, char **argv){
+              return -1;
+        }
+        ```
 * I will unscrupulously add whatever feature I like.
 
 ## Notes ##
--- mdown.py
+++ mdown.py
@@ -1,9 +1,33 @@
+#!/usr/bin/env python
+"""
+Convert markdown to HTML with some cool features.
+
+Options:
+ * -c <cssfile> - use the following CSS file
+ * -h <header>  - use the following header.html
+ * -f <footer>  - use the following footer
+"""
+
 import markdown
-import sys
+import sys, getopt
 import re
 
 
-fname=sys.argv[1]
+try:
+    opts,bargs=getopt.getopt( sys.argv[1:], "c:h:f:");
+    opts=dict(opts)
+    if len( bargs )<1:
+        raise getopt.GetoptError("ERROR: input file")
+    fname=bargs[0]
+    header=opts["-h"] if opts.has_key("-h") else None
+    footer=opts["-f"] if opts.has_key("-f") else None
+    css   =opts["-c"] if opts.has_key("-c") else None
+except getopt.GetoptError, err:
+    print str(err) # will print something like "option -a not recognized"
+    print __doc__
+    sys.exit()
+
+
 f=open(fname, "r")
 content=f.read()
 f.close()
@@ -11,4 +35,17 @@ f.close()
 md=markdown.Markdown(extensions=["pygments"])
 
 # print the HTML
-print md.convert( content )#,  ['codehilite', 'fenced_code'])
+if header:
+    f=open(header,"r")
+    print f.read()
+    f.close()
+
+if css:
+    print '<link rel="stylesheet" type="text/css" href="%s">'%css
+
+print md.convert( content )
+
+if footer:
+    f=open(footer,"r")
+    print f.read()
+    f.close()
